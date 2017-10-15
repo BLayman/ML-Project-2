ce44e7d71bac1862afc76841595cb07705b5435a
Carsen Ball <ballcarsen@gmail.com>
2017-10-14 18:46:40 -0600

    I think it works

--- MLProj2/src/radialBasis/RbNodeHidden.py
+++ MLProj2/src/radialBasis/RbNodeHidden.py
@@ -17,8 +17,10 @@ class RbNodeHidden:
         distance = 0
         for i in range(len(inputVector)):
                 # adds the square of the difference of each variable in the data point
-                distance += math.pow((inputVector[i] - self.mean[i]), 2)
-        phi = math.exp(-distance/(2 * self.sigma * self.sigma))
+                #distance += math.pow((inputVector[i] - self.mean[i]), 2)
+                distance += math.pow((self.mean[i] - inputVector[i] ), 2)
+        #phi = math.exp(-distance/(2 * self.sigma * self.sigma))
+        phi = math.exp(-1 * math.pow((math.pow(distance,.5) * .1/(2 * self.sigma * self.sigma)), 2))
         return phi
             
     
